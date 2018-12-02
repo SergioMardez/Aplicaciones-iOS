6023a07fff639af29fb993d18c0d825643ffce57
Sergio Martín <usermartin@usal.es>
2018-12-02 16:04:52 +0100

    NEw app
    Creacion de la primera app del curso

---
+++ 01-iamrich/01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Sergio on 02/12/2018.
+//  Copyright © 2018 SergioInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
