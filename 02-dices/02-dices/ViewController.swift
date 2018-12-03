ceeb4fa6c20a011ac67e1f81d3a360373f89eea3
Sergio Martín <usermartin@usal.es>
2018-12-03 16:25:52 +0100

    App 2
    Creacion de la segunda app dados

---
+++ 02-dices/02-dices/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  02-dices
+//
+//  Created by Sergio on 03/12/2018.
+//  Copyright © 2018 SergioInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageViewDiceLeft: UIImageView!
+    @IBOutlet weak var imageViewDiceRight: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    //Metodos
+    @IBAction func rollPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func normasPressed(_ sender: UIButton) {
+    }
+    
+}
+
